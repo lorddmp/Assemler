288c5f458e1e1c1b5ca594f9f6259cfb2a5bcecd
lorddmp <dpavlov-07@mail.ru>
2026-04-05 11:47:02 +0300

    Now printf works with %c, %s, %x, %o, %b and %d!

--- PRINTF/haha.c
+++ PRINTF/haha.c
@@ -1,6 +1,6 @@
 extern int my_printf(const char* string, ...);
 
 int main() {
-    my_printf("\"%x\"", 1);
+    int a = my_printf("%c, %s, %x, %o, %b, %d", 'A', "hahaha", 17, 17, 17, 100 + 200);
     return 0;
 }
