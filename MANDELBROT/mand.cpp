b1f41c4077e71d604f58aaf6ecdae47f8b643fe3
lorddmp <dpavlov-07@mail.ru>
2026-04-09 14:56:55 +0300

    printf is over. Mandelbrot works, but very slowly. Optimisation are coming!

--- MANDELBROT/mand.cpp
+++ MANDELBROT/mand.cpp
@@ -1,20 +1,27 @@
 #include <SDL2/SDL.h>
 #include <math.h>
+#include <stdio.h>
 
 #define MAX_ITER  256
-#define MAX_RADIUS 4
+#define MAX_RADIUS 100
+
+struct point:
+{
+
+}
 
 int main() 
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    int width = 1980;
-    int height = 1080;
+    const int width = 1920;
+    const int height = 1080;
+    const float step = 0.01;
 
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     SDL_CreateWindowAndRenderer(width, height, 0, &window, &renderer);
-    SDL_SetWindowTitle(window, "x < y : красный, иначе синий");
+    SDL_SetWindowTitle(window, "Mandelbrot");
 
     SDL_Texture *texture = SDL_CreateTexture(renderer,
                                              SDL_PIXELFORMAT_ARGB8888,
@@ -26,40 +33,46 @@ int main()
     SDL_Event event;
     int running = 1;
 
-    long virtual_x = -540;
-    long virtual_y = -980;
+    float* virtual_x = -9.6;
+    float virtual_y = -5.4;
+    float offset_x = 0;
+    float offset_y = 0;
 
-    int new_x, new_y = 0;
-    int iteration = 0, var = 0;
-    
+    float new_x, new_y, var = 0;
+    float zoom = 1;    
+    long int iteration = 0;
 
     while (running) 
     {
-        for (long y = 0; y < height; y++, virtual_y++) 
+        for (long y = 0; y < height; y++, virtual_y += step * zoom) 
         {
-            for (long x = 0; x < width; x++, virtual_x++) 
+            virtual_y = offset_y + (y - height/2) * step * zoom;
+            for (long x = 0; x < width; x++, virtual_x += step * zoom) 
             {
+                virtual_x = offset_x + (x - width/2) * step * zoom;
                 iteration = 0;
-                new_x = virtual_x;
-                new_y = virtual_y;
+                new_x = 0;
+                new_y = 0;
 
                 for(; iteration < MAX_ITER; iteration++)
                 {
-                    var = new_x*new_x - new_y*new_y;
-                    new_y = 2*new_x*new_y;
+                    var = new_x*new_x - new_y*new_y + virtual_x;
+                    new_y = 2*new_x*new_y + virtual_y;
                     new_x = var;
 
                     if (new_x*new_x + new_y*new_y >= MAX_RADIUS)
                         break;
                 }
 
+                // printf ("iteration = %d\n", iteration);
+                // printf("virtual_x = %lf, y = %lf", virtual_x, virtual_y);
+
                 if (iteration == MAX_ITER) 
                     pixels[y * width + x] = 0xFF000000;
+
                 else
                 {
-                    int r = (iteration * 255) / MAX_ITER;
-                    int b = 255 - r;
-                    int color = 0xFF000000 | (r << 16) | (0 << 8) | b;
+                    int color = 0xFF000000 + (iteration << 30) + (iteration << 20) + (iteration << 10);
                     pixels[y * width + x] = color;
                 }
             }
@@ -70,18 +83,20 @@ int main()
         SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
 
-        while (SDL_PollEvent(&event))
+        while(SDL_PollEvent(&event))
             if (event.type == SDL_QUIT) 
                 running = 0;
 
         const Uint8* keys = SDL_GetKeyboardState(NULL);
-        if (keys[SDL_SCANCODE_W]) virtual_y -= 100;
-        if (keys[SDL_SCANCODE_S]) virtual_y += 100;
-        if (keys[SDL_SCANCODE_A]) virtual_x += 100;
-        if (keys[SDL_SCANCODE_D]) virtual_x -= 100;
+        if (keys[SDL_SCANCODE_W]) offset_y -= 0.1*zoom;
+        if (keys[SDL_SCANCODE_S]) offset_y += 0.1*zoom;
+        if (keys[SDL_SCANCODE_A]) offset_x -= 0.1*zoom;
+        if (keys[SDL_SCANCODE_D]) offset_x += 0.1*zoom;
+        if (keys[SDL_SCANCODE_E]) zoom *= 1.1;
+        if (keys[SDL_SCANCODE_Q]) zoom /= 1.1;
         if (keys[SDL_SCANCODE_ESCAPE]) running = 0;
 
-        }
+    }
 
     free(pixels);
     SDL_DestroyTexture(texture);
--- PRINTF/haha.c
+++ PRINTF/haha.c
@@ -1,6 +1,6 @@
 extern int my_printf(const char* string, ...);
 
 int main() {
-    int a = my_printf("MY PERFECT RESULT: %c, %s, %x, %o, %b, %d", 'A', "hahaha", 17, 17, 17, 100 + 200);
+    int a = my_printf("MY PERFECT RESULT: %d, %s, %x, %o, %b, %d, %d, %c\n", -1, "hahaha", 17, 17, 17, 100, -1000, 'A');
     return 0;
 }
